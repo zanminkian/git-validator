c16b83fea4311b8899e4946a7b0d5858710aaf98
zanminkian <hellozmj@qq.com>
2023-05-06 04:39:29 +0000

    chore: remove `git-hooks`

--- packages/git-hooks/src/eslint.config.cjs
+++
@@ -1,5 +0,0 @@
-const { join } = require('node:path')
-const { cosmiconfigSync } = require('cosmiconfig')
-const config = require('@zanminkian/eslint-config')
-
-module.exports = cosmiconfigSync('eslint').search(join(__dirname, '..'))?.config ?? config
