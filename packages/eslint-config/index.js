e20672607cb1c0e8c2ac497e5910c39a5d817029
zanminkian <hellozmj@qq.com>
2023-06-09 03:22:35 +0000

    chore: clean comments

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -6,9 +6,10 @@ module.exports = {
   // https://github.com/antfu/eslint-config/pull/113
   ignorePatterns: listUnsupportedExtensions(),
   rules: {
-    // We have to add this because of [this](https://github.com/antfu/eslint-config/pull/110) and [this](https://github.com/antfu/eslint-config/pull/144)
+    // https://github.com/antfu/eslint-config/pull/110
+    // https://github.com/antfu/eslint-config/pull/144
     '@typescript-eslint/consistent-type-imports': 'off',
-    // We have to add this because of [this](https://github.com/antfu/eslint-config/pull/145)
+    // https://github.com/antfu/eslint-config/pull/145
     'dot-notation': 'off',
     // https://github.com/antfu/eslint-config/pull/159
     'n/prefer-global/process': ['error', 'never'],
