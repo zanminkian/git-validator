01046b0bac1e51069d683bf9dca89afbe3bbf5a5
zanminkian <hellozmj@qq.com>
2023-07-06 06:57:22 +0000

    refactor(git-validator): optimize the config file

---
+++ .changeset/cuddly-swans-explain.md
@@ -0,0 +1,5 @@
+---
+"git-validator": patch
+---
+
+refactor(git-validator): optimize the config file
--- packages/git-validator/src/eslint.config.cjs
+++ packages/git-validator/src/eslint.config.cjs
@@ -1,5 +1,6 @@
 const { join } = require('node:path')
 const { cosmiconfigSync } = require('cosmiconfig')
-const config = require('@zanminkian/eslint-config')
 
-module.exports = cosmiconfigSync('eslint').search(join(__dirname, '..'))?.config ?? config
+const defaultConfig = { extends: '@zanminkian/eslint-config' }
+
+module.exports = cosmiconfigSync('eslint').search(join(__dirname, '..'))?.config ?? defaultConfig
