debcf8b01cd21e89db7d53b45c1d0054b74e3230
zanminkian <hellozmj@qq.com>
2023-08-19 10:19:14 +0000

    docs(prettier-config): add rethinking comments

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  semi: false,
-  singleQuote: true,
-  printWidth: 120,
+  semi: false, // Rethinking: enabling semi is better because it can make code consistent.
+  singleQuote: true, // SingleQuote should be disabled because HTML usually use double quote.
+  printWidth: 120, // 120 may be too long. Try 100 one day.
 }
