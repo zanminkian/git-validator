e73b5f3d6a7a090450460c6433555999046edcb4
zanminkian <hellozmj@qq.com>
2023-08-22 08:34:05 +0000

    refactor(prettier-config): remove `semi` and `singleQuote` config

---
+++ .changeset/lovely-readers-raise.md
@@ -0,0 +1,6 @@
+---
+"@zanminkian/prettier-config": minor
+"git-validator": minor
+---
+
+refactor(prettier-config): remove `semi` and `singleQuote` config
--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,5 +1,3 @@
 module.exports = {
-  semi: false, // Rethinking: enabling semi is better because it can make code consistent.
-  singleQuote: true, // SingleQuote should be disabled because HTML usually use double quote.
-  printWidth: 120, // 120 may be too long. Try 100 one day.
-}
+  printWidth: 100, // 120 may be too long. Try 100 one day.
+};
