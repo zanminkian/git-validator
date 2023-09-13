4f1aa7302a55aeb961dca40f59fc9affadbdb1b2
zanminkian <hellozmj@qq.com>
2023-09-13 14:21:52 +0000

    perf(prettier-config): parallelly check file

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -3,29 +3,21 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import process from "node:process";
 
-const cwd = process.cwd();
-const tsPath = path.resolve(cwd, "tailwind.config.ts");
-const jsPath = path.resolve(cwd, "tailwind.config.js");
-const jsonPath = path.resolve(cwd, "tailwind.config.json");
-
-const tailwindConfig = (await fs
-  .access(tsPath)
-  .then(() => true)
-  .catch(() => false))
-  ? tsPath
-  : (await fs
-      .access(jsPath)
-      .then(() => true)
-      .catch(() => false))
-  ? jsPath
-  : (await fs
-      .access(jsonPath)
-      .then(() => true)
-      .catch(() => false))
-  ? jsonPath
-  : undefined;
+const configFilePaths = ["js", "ts", "json"].map((i) =>
+  path.resolve(process.cwd(), `tailwind.config.${i}`),
+);
+const promiseFlags = configFilePaths.map(async (filepath) =>
+  fs
+    .access(filepath)
+    .then(() => true)
+    .catch(() => false),
+);
+const flags = await Promise.all(promiseFlags);
+const index = flags.findIndex(Boolean);
 
 export default {
-  ...(tailwindConfig ? { plugins: ["prettier-plugin-tailwindcss"], tailwindConfig } : {}),
+  ...(index >= 0
+    ? { plugins: ["prettier-plugin-tailwindcss"], tailwindConfig: configFilePaths[index] }
+    : {}),
   printWidth: 100, // 120 may be too long
 };
