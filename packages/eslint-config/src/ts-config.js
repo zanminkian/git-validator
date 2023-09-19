d19ba77d602b02a8729c2cefbdca0d84941d446b
zanminkian <hellozmj@qq.com>
2023-09-19 17:29:00 +0000

    feat(eslint-config): turn off no-undef

--- packages/eslint-config/src/ts-config.js
+++ packages/eslint-config/src/ts-config.js
@@ -100,6 +100,7 @@ export default {
   rules: {
     ...jsConfig.rules,
     ...getTsRules(),
+    "no-undef": "off", // https://typescript-eslint.io/linting/troubleshooting/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
 
     // ban some syntaxes to reduce mistakes
     "@typescript-eslint/ban-types": "error",
