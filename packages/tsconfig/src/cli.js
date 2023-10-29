9048463e0bf51291b36a9139a229b5f76075460a
zanminkian <hellozmj@qq.com>
2023-10-29 16:12:54 +0000

    refactor(tsconfig): optimize cli

--- packages/tsconfig/src/cli.js
+++ packages/tsconfig/src/cli.js
@@ -4,7 +4,11 @@ import { resolve } from "node:path";
 import process from "node:process";
 import { Command } from "commander";
 
-const generatingTsconfigContent = `{
+/**
+ * @param {{path: string, name: string, force: boolean}} options
+ */
+async function init(options) {
+  const generatingTsconfigContent = `{
   "extends": "${process.env["TSCONFIG_EXTENDS"] ?? "@git-validator/tsconfig"}",
   "include": ["src"],
   "exclude": ["**/*.spec.ts", "**/*.test.ts"],
@@ -14,6 +18,23 @@ const generatingTsconfigContent = `{
 }
 `;
 
+  const { path, name, force } = options;
+  const fullName = resolve(process.cwd(), path, name);
+  if (
+    !(await fs
+      .access(fullName)
+      .then(() => true)
+      .catch(() => false)) ||
+    force
+  ) {
+    await fs.writeFile(fullName, generatingTsconfigContent);
+  } else {
+    throw new Error(
+      `${fullName} is already existing! You can apply --force option to overwrite it.`,
+    );
+  }
+}
+
 const program = new Command();
 program.name("tsconfig");
 program
@@ -22,21 +43,6 @@ program
   .option("-p, --path <path>", "directory path to generate file to", ".")
   .option("-n, --name <filename>", "tsconfig file name", "tsconfig.json")
   .option("-f, --force", "forcefully overwrite existing file")
-  .action(async ({ path, name, force }) => {
-    const fullName = resolve(process.cwd(), path, name);
-    if (
-      !(await fs
-        .access(fullName)
-        .then(() => true)
-        .catch(() => false)) ||
-      force
-    ) {
-      await fs.writeFile(fullName, generatingTsconfigContent);
-    } else {
-      throw new Error(
-        `${fullName} is already existing! You can apply --force option to overwrite it.`,
-      );
-    }
-  });
+  .action(init);
 
 program.parse();
