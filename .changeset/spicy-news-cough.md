05feb6da3aed115df9ad0ee6c1a54cff83491858
zanminkian <hellozmj@qq.com>
2023-11-19 20:13:46 +0000

    fix(git-validator): use stable api to mute warning from node

---
+++ .changeset/spicy-news-cough.md
@@ -0,0 +1,5 @@
+---
+"git-validator": patch
+---
+
+fix(git-validator): use stable api to mute warning from node
--- packages/git-validator/src/bin/eslint.js
+++ packages/git-validator/src/bin/eslint.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
+// @ts-check
+import fs from "node:fs/promises";
 import { createRequire } from "node:module";
 import { resolve } from "node:path";
-import packageJson from "eslint/package.json" assert { type: "json" };
 
-function getBinPath() {
+async function getBinPath() {
   const requireResolve = createRequire(import.meta.url).resolve;
-  const eslintPath = requireResolve("eslint/package.json").slice(0, -"/package.json".length);
-  const bin = typeof packageJson.bin === "string" ? packageJson.bin : packageJson.bin.eslint;
-  return resolve(eslintPath, bin);
+  const packageJsonPath = requireResolve("eslint/package.json");
+  const packageJson = JSON.parse(await fs.readFile(packageJsonPath, "utf8"));
+  const eslintPath = packageJsonPath.slice(0, -"/package.json".length);
+  const binPath = typeof packageJson.bin === "string" ? packageJson.bin : packageJson.bin.eslint;
+  return resolve(eslintPath, binPath);
 }
 
 // eslint-disable-next-line @git-validator/no-dynamic-import
-await import(getBinPath());
+await import(await getBinPath());
