264e8916cdf1abd6adac1d413902788d77659c4a
zanminkian <hellozmj@qq.com>
2024-02-24 17:37:03 +0000

    chore: sort keys

--- packages/eslint-config/src/js-config/index.js
+++ packages/eslint-config/src/js-config/index.js
@@ -10,6 +10,10 @@ export default {
     // 1. standard config rules
     ...standardConfig.rules,
     "dot-notation": "off", // disable it as it conflicts with ts when enabling ts-check
+    "promise/param-names": [
+      "error",
+      { resolvePattern: "^_?res(olve)?$", rejectPattern: "^_?rej(ect)?$" },
+    ],
 
     // 2. code style for a better readability
     "arrow-body-style": ["error", "as-needed"],
@@ -24,10 +28,22 @@ export default {
     // "unicorn/number-literal-case": "error", // 0XFF -> 0xFF // conflict with prettier
 
     // 3. ban some syntaxes to reduce mistakes
-    "promise/param-names": [
+    "func-name-matching": "error",
+    "no-duplicate-imports": "error",
+    "no-implicit-coercion": [
       "error",
-      { resolvePattern: "^_?res(olve)?$", rejectPattern: "^_?rej(ect)?$" },
-    ],
+      { disallowTemplateShorthand: true, allow: ["!!"] },
+    ], // forbid code like `const num = +str`;
+    "no-invalid-this": "error",
+    "no-multi-assign": "error",
+    "no-plusplus": "error",
+    "no-shadow": ["error", { ignoreOnInitialization: true }],
+    "no-unused-private-class-members": "error",
+    "prefer-exponentiation-operator": "error",
+    // fp
+    "fp/no-arguments": "error",
+    "fp/no-delete": "error",
+    // import
     "import/no-commonjs": [
       "error",
       {
@@ -36,16 +52,18 @@ export default {
         allowPrimitiveModules: false,
       },
     ],
-    "import/no-self-import": "error",
     "import/no-dynamic-require": "error",
-    "import/no-relative-packages": "error", // forbid to import module from other monorepo packages by relative paths
     "import/no-mutable-exports": "error", // forbid code like `export let count = 3`
+    "import/no-relative-packages": "error", // forbid to import module from other monorepo packages by relative paths
+    "import/no-self-import": "error",
     // "import/no-named-as-default-member": "error", // forbid code like `import foo from './foo.js'; const bar = foo.bar;`
-    "fp/no-arguments": "error",
-    "fp/no-delete": "error",
+    "n/no-sync": "error",
     "n/prefer-global/process": ["error", "never"],
     "n/prefer-global/buffer": ["error", "never"],
-    "n/no-sync": "error",
+    // react
+    "react-hooks/exhaustive-deps": "error",
+    "react-hooks/rules-of-hooks": "error",
+    // unicorn
     "unicorn/error-message": "error",
     "unicorn/no-instanceof-array": "error",
     "unicorn/no-new-array": "error",
@@ -55,22 +73,6 @@ export default {
     "unicorn/prefer-number-properties": "error",
     "unicorn/throw-new-error": "error",
     // 'unicorn/no-null': 'error', // null can be useful when interact with json.
-    "func-name-matching": "error",
-    "no-implicit-coercion": [
-      "error",
-      { disallowTemplateShorthand: true, allow: ["!!"] },
-    ], // forbid code like `const num = +str`;
-    "no-invalid-this": "error",
-    "no-multi-assign": "error",
-    "no-plusplus": "error",
-    "no-duplicate-imports": "error",
-    "no-shadow": ["error", { ignoreOnInitialization: true }],
-    "no-unused-private-class-members": "error",
-    "prefer-exponentiation-operator": "error",
-
-    // react
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
 
     "@git-validator/import-regex": "error",
     "@git-validator/no-dynamic-import": "error",
