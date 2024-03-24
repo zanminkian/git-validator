4cccab4908267a5fdda248bb5fc4d3d822a02a8b
zanminkian <hellozmj@qq.com>
2024-03-24 16:09:43 +0000

    test: add test case

---
+++ packages/eslint-plugin/test/no-instanceof-builtin.spec.ts
@@ -0,0 +1,13 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+import types from "node:util/types";
+
+await describe("no-instanceof-builtin", async () => {
+  await it("node:utils/types should have 42 methods", () => {
+    const keys = Object.keys(types);
+    const propertyNames = Object.getOwnPropertyNames(types);
+    assert.strictEqual(keys.length, 42);
+    assert.strictEqual(propertyNames.length, 42);
+    assert.deepEqual(keys, propertyNames);
+  });
+});
