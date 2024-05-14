03b51e2a379bcc833e423d12471e065769119e61
zanminkian <hellozmj@qq.com>
2024-05-14 10:36:06 +0000

    fix(eslint-config): respect the non-root `.gitignore` file

---
+++ .changeset/dirty-singers-destroy.md
@@ -0,0 +1,5 @@
+---
+"@git-validator/eslint-config": patch
+---
+
+fix(eslint-config): respect the non-root `.gitignore` file
--- packages/eslint-config/src/gitignore-config.ts
+++ packages/eslint-config/src/gitignore-config.ts
@@ -1,20 +1,19 @@
-import fs from "node:fs/promises";
-import path from "node:path";
-import process from "node:process";
-import { gitignoreToMinimatch } from "@humanwhocodes/gitignore-to-minimatch";
+import childProcess from "node:child_process";
+import { promisify } from "node:util";
+
+const exec = promisify(childProcess.exec);
 
 async function globallyIgnore() {
-  const ignores = (
-    await fs
-      .readFile(path.resolve(process.cwd(), ".gitignore"), "utf-8")
-      .catch(() => "")
-  )
-    .split("\n")
-    .map((i) => i.trim())
-    .filter(Boolean)
-    .filter((i) => !i.startsWith("#"))
-    .map((i) => gitignoreToMinimatch(i));
-  // Globally ignore. https://eslint.org/docs/latest/use/configure/configuration-files-new#globally-ignoring-files-with-ignores
+  const { stdout } = await exec(
+    "git ls-files --others --ignored --exclude-standard --directory",
+  ).catch(() => {
+    console.warn(
+      "Warn: Running `git ls-files` fail. We recommend to run `git init` to setup the project first.",
+    );
+    return { stdout: "" };
+  });
+  const ignores = stdout.split("\n").filter(Boolean);
+  // https://eslint.org/docs/latest/use/configure/configuration-files#specifying-files-and-ignores
   return { ignores };
 }
 
