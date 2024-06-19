745e5a73440016a81f5bc910d431aaaa98028dcc
zanminkian <hellozmj@qq.com>
2024-06-19 03:54:53 +0000

    feat(git-validator): support to print json format on analyze command

---
+++ .changeset/hip-eyes-hang.md
@@ -0,0 +1,5 @@
+---
+"git-validator": patch
+---
+
+feat(git-validator): support to print json format on analyze command
--- packages/git-validator/src/bin/git-validator.js
+++ packages/git-validator/src/bin/git-validator.js
@@ -77,6 +77,11 @@ program
 program
   .command("analyze")
   .description("analyze js/ts project quality and print the report")
+  .option(
+    "-f, --format <type>",
+    'specify the analysis format, which can be "table" or "json"',
+    "table",
+  )
   .option("-d, --detail", "show analysis detail")
   .option(
     "-i, --ignore <path>",
@@ -114,34 +119,63 @@ program
         console.log("Meta Property", i);
       });
     }
-    console.log("1. Code lines and files count:");
-    console.table({
-      "Code Lines": analysis.codeLines,
-      "TS Files": analysis.tsFiles,
-      "JS Files": analysis.jsFiles,
-      "Analyzed Files": analysis.analyzedFiles,
-    });
-    console.log("2. Type flaws count:");
-    console.table({
-      "Any Types": analysis.anyTypes.length,
-      Assertions: analysis.assertions.length,
-      "Non-null Assertions": analysis.nonNullAssertions.length,
-    });
-    console.log("3. Code style flaws count:");
-    console.table({
-      "Renamed Imports": analysis.renamedImports.length,
-      "Import Expressions": analysis.importExpressions.length,
-      "Instanceof Operators": analysis.instanceofOperators.length,
-    });
-    console.log("4. Module interop issues count:");
-    console.table({
-      "Export Defaults": analysis.exportDefaults.length,
-    });
-    console.log("5. Cross-platform issues count:");
-    console.table({
-      "Node Protocol Imports": analysis.nodeProtocolImports.length,
-      "Meta Properties": analysis.metaProperties.length,
-    });
+    if (options.format === "json") {
+      console.log(
+        JSON.stringify({
+          // 1. Code lines and files count:
+          codeLines: analysis.codeLines,
+          tsFiles: analysis.tsFiles,
+          jsFiles: analysis.jsFiles,
+          analyzedFiles: analysis.analyzedFiles,
+
+          // 2. Type flaws count:
+          anyTypes: analysis.anyTypes.length,
+          assertions: analysis.assertions.length,
+          nonNullAssertions: analysis.nonNullAssertions.length,
+
+          // 3. Code style flaws count:
+          renamedImports: analysis.renamedImports.length,
+          importExpressions: analysis.importExpressions.length,
+          instanceofOperators: analysis.instanceofOperators.length,
+
+          // 4. Module interop issues count:
+          exportDefaults: analysis.exportDefaults.length,
+
+          // 5. Cross-platform issues count:
+          nodeProtocolImports: analysis.nodeProtocolImports.length,
+          metaProperties: analysis.metaProperties.length,
+        }),
+      );
+    } else {
+      console.log("1. Code lines and files count:");
+      console.table({
+        "Code Lines": analysis.codeLines,
+        "TS Files": analysis.tsFiles,
+        "JS Files": analysis.jsFiles,
+        "Analyzed Files": analysis.analyzedFiles,
+      });
+      console.log("2. Type flaws count:");
+      console.table({
+        "Any Types": analysis.anyTypes.length,
+        Assertions: analysis.assertions.length,
+        "Non-null Assertions": analysis.nonNullAssertions.length,
+      });
+      console.log("3. Code style flaws count:");
+      console.table({
+        "Renamed Imports": analysis.renamedImports.length,
+        "Import Expressions": analysis.importExpressions.length,
+        "Instanceof Operators": analysis.instanceofOperators.length,
+      });
+      console.log("4. Module interop issues count:");
+      console.table({
+        "Export Defaults": analysis.exportDefaults.length,
+      });
+      console.log("5. Cross-platform issues count:");
+      console.table({
+        "Node Protocol Imports": analysis.nodeProtocolImports.length,
+        "Meta Properties": analysis.metaProperties.length,
+      });
+    }
   });
 
 setup(program, {
