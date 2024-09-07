7310a8de5c59728a86b38f2856dc9cc948d641c4
zanminkian <hellozmj@qq.com>
2024-09-07 11:24:12 +0000

    chore: update

--- packages/types/src/json-parse.d.ts
+++ packages/types/src/json-parse.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/method-signature-style */
 interface JSON {
   /**
    * Converts a JavaScript Object Notation (JSON) string into an object.
@@ -5,7 +6,6 @@ interface JSON {
    * @param reviver A function that transforms the results. This function is called for each member of the object.
    * If a member contains nested objects, the nested objects are transformed before the parent object is.
    */
-  // eslint-disable-next-line @typescript-eslint/method-signature-style
   parse(
     text: string,
     reviver?: (this: unknown, key: string, value: unknown) => unknown,
