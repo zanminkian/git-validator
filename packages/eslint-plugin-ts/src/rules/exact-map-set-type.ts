4a8d62fdcccb57fe43e7e806c15ac599b62e400f
zanminkian <hellozmj@qq.com>
2024-09-16 16:33:24 +0000

    refactor(eslint-plugin-ts): optimize selector

--- packages/eslint-plugin-ts/src/rules/exact-map-set-type.ts
+++ packages/eslint-plugin-ts/src/rules/exact-map-set-type.ts
@@ -4,14 +4,17 @@ import { createSimpleRule, getRuleName } from "../utils.js";
 export const exactMapSetType = createSimpleRule({
   name: getRuleName(import.meta.url),
   message: "Disallow using Map and Set without type arguments.",
-  create: (context) => ({
-    "Identifier[name=/^(Set|Map|WeakSet|WeakMap)$/][parent.type='NewExpression'][parent.arguments.length=0][parent.typeArguments=undefined]":
-      (node: Node) => {
+  create: (context) => {
+    const selectors = [
+      // new Set();
+      "Identifier[name=/^(Set|Map|WeakSet|WeakMap)$/][parent.type='NewExpression'][parent.arguments.length=0][parent.typeArguments=undefined]",
+      // class Foo {foo: Set}
+      "Identifier[name=/^(Set|Map|WeakSet|WeakMap)$/][parent.type!='NewExpression'][parent.typeArguments=undefined]",
+    ];
+    return {
+      [`:matches(${selectors.join(", ")})`]: (node: Node) => {
         context.reportNode(node);
       },
-    "Identifier[name=/^(Set|Map|WeakSet|WeakMap)$/][parent.type!='NewExpression'][parent.typeArguments=undefined]":
-      (node: Node) => {
-        context.reportNode(node);
-      },
-  }),
+    };
+  },
 });
