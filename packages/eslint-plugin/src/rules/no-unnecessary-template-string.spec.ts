cf53c4960e9f2c92a8ccadeebdc48d000c3ba0b4
zanminkian <hellozmj@qq.com>
2024-09-17 18:44:49 +0000

    test: add test case

--- packages/eslint-plugin/src/rules/no-unnecessary-template-string.spec.ts
+++ packages/eslint-plugin/src/rules/no-unnecessary-template-string.spec.ts
@@ -11,6 +11,15 @@ const valid = [
   "`a\nbc`",
 ];
 
-const invalid = ["``", "`abc`", "`abc\\n`", "`\\nabc`", "`a\\nbc`"];
+const invalid = [
+  // Currently, tagged template string should be reported as well.
+  // Moving it to `valid` part is also reasonable.
+  "outdent`foo`",
+  "``",
+  "`abc`",
+  "`abc\\n`",
+  "`\\nabc`",
+  "`a\\nbc`",
+];
 
 test({ valid, invalid, ...noUnnecessaryTemplateString });
